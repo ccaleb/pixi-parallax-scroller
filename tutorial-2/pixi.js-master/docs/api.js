a14c0e45df18fd0a3ceb3a51af8278f8eb380e18
Christopher Caleb <ccaleb@hotmail.com>
2013-11-05 23:32:54 +0000

    Source code for part 2 of the Pixi.js and JavaScript parallax scroller tutorial.

---
+++ tutorial-2/pixi.js-master/docs/api.js
@@ -0,0 +1,45 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "AssetLoader",
+        "BaseTexture",
+        "BitmapFontLoader",
+        "BitmapText",
+        "CanvasGraphics",
+        "CanvasRenderer",
+        "Circle",
+        "CustomRenderable",
+        "DisplayObject",
+        "DisplayObjectContainer",
+        "Ellipse",
+        "Graphics",
+        "ImageLoader",
+        "InteractionData",
+        "InteractionManager",
+        "JsonLoader",
+        "MovieClip",
+        "Point",
+        "Polygon",
+        "Rectangle",
+        "RenderTexture",
+        "Spine",
+        "Sprite",
+        "SpriteSheetLoader",
+        "Stage",
+        "Text",
+        "Texture",
+        "TilingSprite",
+        "WebGLBatch",
+        "WebGLRenderer"
+    ],
+    "modules": [
+        "PIXI"
+    ],
+    "allModules": [
+        {
+            "displayName": "PIXI",
+            "name": "PIXI"
+        }
+    ]
+} };
+});
